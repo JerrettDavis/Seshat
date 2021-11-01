08e3388e2bdf7f8e732fbdbfbfe45a9ccea8f62b
Jerrett Davis <mxjerrett@gmail.com>
2021-10-31 21:43:20 -0500

    Trying to get code coverage to play nice with webstorm.

--- src/WebUI/ClientApp/src/app/modules/user-printers/pages/users-printers/users-printers.component.spec.ts
+++ src/WebUI/ClientApp/src/app/modules/user-printers/pages/users-printers/users-printers.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersPrintersComponent } from './users-printers.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('UsersPrintersComponent', () => {
   let component: UsersPrintersComponent;
@@ -8,7 +11,12 @@ describe('UsersPrintersComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UsersPrintersComponent ]
+      declarations: [ UsersPrintersComponent ],
+      imports: [
+        MatButtonModule,
+        MatIconModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   });
